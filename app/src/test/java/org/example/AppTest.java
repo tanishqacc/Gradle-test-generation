4a3580c7beb24e270c4e90052ed1cdeed986a38c
tanishq <tanishq.chouhan@bounteous.com>
2024-12-16 16:42:07 +0530

    Gradle-Test-Generation

---
+++ app/src/test/java/org/example/AppTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    public void testMain() {
+        // TODO: Implement test for main
+    }
+
+    @Test
+    public void testGetGreeting() {
+        // TODO: Implement test for getGreeting
+    }
+
+}
